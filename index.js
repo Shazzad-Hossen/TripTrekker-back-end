23146690b6d214b6b19d50fa27642cf50c07af86
Shazzad Hossen <tipsfire.com@gmail.com>
2023-06-01 23:34:47 +0600

    basic structure added

---
+++ index.js
@@ -0,0 +1,21 @@
+const express= require('express');
+const cors= require('cors');
+const port= process.env.PORT || 5000;
+const app= express();
+
+
+//MiddleWare
+app.use(cors());
+app.use(express.json());
+
+
+
+
+app.get('/',(req,res)=>{
+    res.send('Backend is running');
+});
+
+app.listen(port,()=>{
+    console.log(`server is running at http://localhost:${port}`);
+
+})
