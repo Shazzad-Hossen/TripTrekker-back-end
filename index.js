e860948ea4f13dddd8ba9550a5f97e363d6f9709
Firebase 2420 <firebase2420@gmail.com>
2023-07-09 23:38:51 +0600

    mongodb setup done with two routed . create user , and get user data

--- index.js
+++ index.js
@@ -2,12 +2,64 @@ const express= require('express');
 const cors= require('cors');
 const port= process.env.PORT || 5000;
 const app= express();
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://shazzadsrv:NcWApKBJNFwcGspo@cluster0.gaslm7s.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
 
 //MiddleWare
 app.use(cors());
 app.use(express.json());
 
+
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("TripTrekker");
+    const usersCollection = database.collection("users");
+
+    //create user
+    app.post('/user', async(req,res)=>{
+        const data=req.body;
+        const query = { email: data.email };
+        const findRes = await usersCollection.findOne(query);
+        if(findRes){
+            res.send({"acknowledged":true, "user":findRes});
+            return;
+        }
+        const result = await usersCollection.insertOne(data);
+        const resultData = await usersCollection.findOne(query);
+        res.send({...result,"user":resultData});   
+    });
+
+    app.get('/user/:email',async(req,res)=>{
+        const query = { email: req.params.email };
+        const result = await usersCollection.findOne(query);
+        res.send(result);
+    
+    })
+
+
+
+
+
+
+
+   
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+   
+    //await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 
